93ae22fa8ab3b502baee0b5f2086ea0921a1667a
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2022-11-21 00:09:04 +0530

    some changes

--- js1.js
+++ js1.js
@@ -15,3 +15,16 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
