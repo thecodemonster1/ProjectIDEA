9cab3f7bc2ea75c27bc30172aaf07ee2a475f205
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2022-11-26 14:41:15 +0530

    turn html file to php file

--- loginPage.php
+++ loginPage.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -157,14 +161,41 @@
                     <label>Username</label>
                   </div>
                   <div class="user-box">
-                    <input type="password" name="" required="">
+                    <input type="password" name="PasswordText" required="">
                     <label>Password</label>
                   </div>
-                  <button type="submit">
+                  <button type="submit" name="submitBtn">
                     LogIn
                   </button>
                 </form>
-              </div>
+            </div>
+
+            <?php 
+            if(isset($_POST["submitBtn"])){
+                // Read values from the text feild 
+                $Username = $_POST["UsernameText"];
+                $Password = $_POST["PasswordText"];
+                $valid = false;
+                $con = mysqli_connect("localhost","root","","projectmchDB");
+                if(!$con){ // Error Handling part 
+                    alert("Could not connect to DB server. Please try again");
+                }
+                $sql = " SELECT * FROM `userTable` WHERE  `email` = '".$Username."' and  `password` = '".$Password."'";
+                $result = mysqli_query($con,$sql);
+                if(mysqli_num_rows($result) > 0){
+                    $valid = true;
+                } else {
+                    $valid = false;
+                }
+                if($valid) {
+                $_SESSION["userName"] = $Username;
+                header("Location:HomePage.php");
+                } else {
+                echo "Please check your Username and password";
+                }
+            } 
+          ?>
+
         </div>
 
     </body>
