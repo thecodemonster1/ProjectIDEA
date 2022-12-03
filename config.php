c91b45507461b6b7ce6d5076f3f4995ea52a5527
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2022-12-03 09:27:59 +0530

    delete the config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost","root","","projectmchDB");
-    if(!$con){ // Error Handling part 
-        die("Could not connect to DB server. Please try again");
-    }
-?>
